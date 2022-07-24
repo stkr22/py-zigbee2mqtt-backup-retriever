0bb3dd8c628426ddca4b3cd3d6a2a4e71d583a93
stkr22 <checkout@github.com>
2022-07-24 22:30:26 +0200

    init;

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# py-zigbee2mqtt-backup-retriever
-Goal is to provide a simple way to download and store the base64 encoded zip from Zigbee2MQTT backup function on disk.
+# MQTT Retriever
+
+Goal is to provide a simple way to download and store the base64 encoded zip from Zigbee2MQTT backup function on disk. It first request the zip file from mqtt via sending a message to the topic and then waits for a new message on the respone topic. Once it received the message it decodes it and stored it on disk as a zip file.
+
+Sources:
+
+- <https://stackoverflow.com/questions/22402679/write-decoded-from-base64-string-to-file>
+- <https://stackoverflow.com/questions/61043898/how-to-handle-the-loop-stop-paho-mqtt>
+- <https://python.plainenglish.io/mqtt-beginners-guide-in-python-38590c8328ae>
+- <http://www.steves-internet-guide.com/into-mqtt-python-client/>
+- <https://mosquitto.org/man/mosquitto_sub-1.html>
+- <http://www.steves-internet-guide.com/mosquitto_pub-sub-clients/>
+- <https://github.com/Koenkk/zigbee2mqtt/discussions/12731>
+- <https://www.zigbee2mqtt.io/guide/usage/mqtt_topics_and_messages.html#zigbee2mqtt-bridge-request>
---
+++ src/zigbee2mqtt_backup_retriever.py
@@ -0,0 +1,52 @@
+import paho.mqtt.client as mqtt
+import base64
+import logging
+import json
+import time
+from datetime import datetime
+import os
+
+logger = logging.getLogger("MQTT Retriever")
+logging.basicConfig(level=logging.INFO)
+retrieval_end = False
+
+def on_message(client: mqtt.Client, userdata, msg):
+    global retrieval_end
+    filepath = os.getenv('ZIP_FILEPATH', f"zigbee2mqtt_backup_{datetime.now().strftime('%Y%m%d%H%M')}.zip")
+    decoded_message = str(msg.payload.decode("utf-8"))
+    msg = json.loads(decoded_message)
+    if msg["status"].lower() == "ok":
+        logger.info("Message retrieval successful.")
+        decoded = base64.b64decode(msg["data"]["zip"])
+        with open(filepath, 'wb') as fout:
+            fout.write(decoded)
+        logger.info("Base64 string decoded and stored as zip at: %s", filepath)
+        retrieval_end = True
+    else:
+        logger.error("An error occured.")
+        retrieval_end = True
+
+
+client = mqtt.Client("zigbee2mqtt_backup_retriever")
+client.connect(os.getenv('MQTT_HOST', 'localhost'), 1883)
+msg = ""
+info = client.publish(
+    topic='zigbee2mqtt/bridge/request/backup',
+    payload=msg.encode('utf-8'),
+    qos=0,
+)
+client.message_callback_add('zigbee2mqtt/bridge/response/backup', on_message)
+client.subscribe('zigbee2mqtt/bridge/response/backup')
+
+client.loop_start()
+logger.info("Message is published: %s", info.is_published())
+
+timeout_counter = 0
+while not retrieval_end:
+    timeout_counter += 1
+    if timeout_counter > 30:
+        break
+    time.sleep(2)
+client.unsubscribe('zigbee2mqtt/bridge/response/backup')
+client.disconnect()
+client.loop_stop()
